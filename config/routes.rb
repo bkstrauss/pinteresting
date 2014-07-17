0984eca956e595594deb611506dc804b8237e71e
Ben Strauss <bkstrauss@gmail.com>
2014-07-17 16:59:24 -0400

    added challenges page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "challenges" => "pages#challenges"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
