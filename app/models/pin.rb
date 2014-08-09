f0fad88ad0c543cf01c2fb3a84fe6ad04d283172
Ben Strauss <bkstrauss@gmail.com>
2014-08-09 14:30:44 -0400

    progress with attaching pdf

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,8 +1,8 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/pdf"]
+	has_attached_file :image
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "application/pdf"]
 
 	validates :description, presence: true
 	validates :image, presence: true
