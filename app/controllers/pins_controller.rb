001cd4119a503bc79d2376075891e12283713699
Ben Strauss <bkstrauss@gmail.com>
2014-08-10 23:59:53 -0400

    added date submitted

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -20,7 +20,7 @@ class PinsController < ApplicationController
   def create
     @pin = current_user.pins.build(pin_params)
     if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.'
+      redirect_to pins_path, notice: 'Pin was successfully created.'
     else
       render :new
     end
